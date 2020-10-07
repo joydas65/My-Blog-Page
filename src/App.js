139bd8c8efe41051097ad40213c88fca3df9e62c
Joy Das <35962760+joydas65@users.noreply.github.com>
2020-10-07 10:47:16 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,12 +5,20 @@ import {
 } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import ArticlePage from './pages/ArticlePage';
+import ArticlesList from './pages/ArticlesList';
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <Route path="/" component={HomePage} exact />
+        <div id="page-body">
+          <Route path="/" component={HomePage} exact />
+          <Route path="/about" component={AboutPage} />
+          <Route path="/articles-list" component={ArticlesList} />
+          <Route path="/article" component={ArticlePage} />
+        </div>
       </div>
     </Router>
   );
