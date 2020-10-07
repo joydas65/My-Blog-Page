df17714bcfbb0a3df74d6dabd782ca20975a3ec3
Joy Das <35962760+joydas65@users.noreply.github.com>
2020-10-07 09:02:32 +0000

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+} from 'react-router-dom';
+import './App.css';
+import HomePage from './pages/HomePage';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Route path="/" component={HomePage} exact />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
