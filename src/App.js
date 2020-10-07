ca1d0c5a1a602b0d1cc8184a6195ae6de9707315
Joy Das <35962760+joydas65@users.noreply.github.com>
2020-10-07 16:56:35 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import './App.css';
 import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
 import ArticlePage from './pages/ArticlePage';
-import ArticlesList from './pages/ArticlesList';
+import ArticlesListPage from './pages/ArticlesListPage';
 import NavBar from './Navbar';
 
 function App() {
@@ -18,7 +18,7 @@ function App() {
         <div id="page-body">
           <Route path="/" component={HomePage} exact />
           <Route path="/about" component={AboutPage} />
-          <Route path="/articles-list" component={ArticlesList} />
+          <Route path="/articles-list" component={ArticlesListPage} />
           <Route path="/article/:name" component={ArticlePage} />
         </div>
       </div>
